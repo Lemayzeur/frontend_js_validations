b67dd1028303597327359256750e40ca06d97a61
Code9 Class <code9_class@lemayzeur.lemayzeur.com>
2021-12-18 14:40:44 -0500

    First commit

---
+++ hello.js
@@ -0,0 +1,39 @@
+const submitHandler = (event) => {
+    event.preventDefault();
+
+    let inputs = document.querySelectorAll("input");
+
+    inputs.forEach(input => {
+        let errors = validateField(input);
+    })
+}
+
+const validateField = (input) => {
+    const { value } = input;
+    let errors = [];
+
+    if(!value | value < 1){
+        errors.push('chan sa obligatwa');
+    }
+
+    let displayBox = input.nextElementSibling;
+
+    displayBox.innerHTML = "";
+    if (errors.length > 0){  
+        errors.forEach(er => displayBox.innerHTML += er)      
+        displayBox.style.display = "inline-block";       
+    }else{
+        displayBox.style.display = "none";   
+    }
+}
+
+const updateFieldHandler = (event) => {
+    let errors = validateField(event.target)
+}
+
+document.getElementById('form').addEventListener('submit', submitHandler);
+
+let inputs = document.querySelectorAll("input");
+inputs.forEach(input => {
+    input.addEventListener('input', updateFieldHandler, false)
+})
